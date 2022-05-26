25b9a87864c0c19a864b73c4dc9d01361350ddce
Gokulcasper <ronitcasper215@gmail.com>
2022-05-26 17:40:34 +0530

    Feat: Operations & Operators

---
+++ Style.js
@@ -0,0 +1,51 @@
+var a = "10";
+var b = 20;
+a = Number(a); // Type Coercion
+console.log(a + b);
+
+//Arithmetic Operations
+
+var a = 10;
+var b = 20;
+var c;
+c = a + b;
+c = a - b;
+c = a / b;
+c = a % b;
+c = a * b;
+c = 2 ** 2;
+c = ++a;
+c = --b;
+
+console.log(c);
+
+// Assignment Operator
+
+var a = 10;
+
+a += 3;
+a -= 3;
+a /= 3;
+a *= 3;
+a %= 3;
+
+console.log(a);
+
+// Math function
+
+var c;
+
+c = Math.PI;
+c = Math.E;
+c = Math.round(2.3);
+c = Math.ceil(2.3);
+c = Math.floor(3.2);
+c = Math.sqrt(20);
+c = Math.trunc(3.45);
+c = Math.abs(-85);
+c = Math.pow(2, 4);
+c = Math.min(2, 4, 6, 34, 5);
+c = Math.max(43, 54, 5, 55);
+c = Math.sign(-20);
+c = Math.random();
+c = Math.floor(round() * 100 + 1);
---
+++ style2.js
@@ -0,0 +1,31 @@
+// Comparision Operator
+
+var a = "10";
+var b = 10;
+
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+
+//Relational Operator
+
+var a = 10;
+var b = 20;
+
+console.log("Greatest :", a > b);
+console.log("lowest :", a < b);
+console.log("Greater than or Equal to :", a >= b);
+console.log("Lesser Than or Equal to :", a <= b);
+
+//Logical Operator
+
+var mark = 35;
+
+console.log(mark >= 35 && mark <= 100);
+
+var a = 10;
+
+console.log(a == 5 || a == 10);
+
+a = false;
+console.log(!a);
